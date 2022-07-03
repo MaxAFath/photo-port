7a673f8ab9924bfeeddbd61836d6baf655938b5c
Max Fathauer <maxfathauer@gmail.com>
2022-07-02 21:06:40 -0400

    add stuff

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
 
   return (
     <div>
-      <Nav></Nav>
+      <Nav
+      categories={categories}
+      setCurrentCategory={setCurrentCategory}
+      currentCategory={currentCategory}></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
     </div>
